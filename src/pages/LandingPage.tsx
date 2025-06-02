aae55ab5212c796427daff890562b1eb0d3e3122
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-06-02 16:17:26 -0400

    update logo

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/software-solutions.png";
+import logo from "../assets/logo.png";
 import NavBarComponent from "../components/NavBarComponent";
 
 export default function LandingPage() {
