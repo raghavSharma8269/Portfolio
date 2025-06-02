b968749c5fdad85438720e5c7aee60f7e92db891
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-06-02 11:41:40 -0400

    removed unused imports

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import NavBarComponent from "../components/NavBarComponent";
 
 const ContactPage = () => {
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/software-solutions.png";
 import NavBarComponent from "../components/NavBarComponent";
