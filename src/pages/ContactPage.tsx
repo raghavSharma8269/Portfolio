8dc79b0857f51db1db67205f46b08e83d66bbb7c
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-06-02 21:09:53 -0400

    small changes

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -6,8 +6,10 @@ const ContactPage = () => {
       <NavBarComponent />
       <div className="container mt-4">
         <div className="text-center">
-          <h1>Contact Us</h1>
-          <p>If you have any questions, feel free to reach out!</p>
+          <h1 style={{ color: "#14E0A0" }}>Contact Us</h1>
+          <p className="mt-4">
+            If you have any questions, feel free to reach out!
+          </p>
         </div>
         <div className="d-flex justify-content-center">
           <form className="mt-4 w-50 w-md-50">
